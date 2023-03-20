5f114fb79eac54dad54735723f000ed73a944166
Kafilat <kafilatadewumi61@gmail.com>
2023-03-20 11:59:04 +0100

    add redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <h1>Todo</h1>
+     <form>
+      <input type="text" placeholder='add todo' />
+      <button>Add</button>
+     </form>
+    </div>
   )
 }
 
